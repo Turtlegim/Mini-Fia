0cbb57cc08f4bc0fd506560afa7dfc8d2662e86c
Mun-Seong <0611s@naver.com>
2021-03-29 15:23:19 +0900

    <backup project>

---
+++ Source/MiniFia/MeshMerge.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MeshMerge.h"
+
+// Sets default values
+AMeshMerge::AMeshMerge()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMeshMerge::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMeshMerge::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
